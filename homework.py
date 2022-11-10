598dfa39d2078cb7b25590e1d96b1a8ba4e667f6
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-11-10 23:27:03 +0500

    all the task except for last one are done

--- homework.py
+++ homework.py
@@ -18,3 +18,28 @@ else:
     while n <= 0:
         print(n)
         n = n + 1
+
+# Python program to iterate over characters of a string
+
+# Code #1
+string_name = "geeksforgeeks"
+
+# Iterate over the string
+for element in string_name:
+    print(element, end=' ')
+print("\n")
+
+# Code #2
+string_name = "GEEKS"
+
+# Iterate over index
+for element in range(0, len(string_name)):
+    print(string_name[element])
+
+# Python program to iterate over characters of a string
+
+string_name = "Geeks"
+
+# Iterate over the string
+for i, v in enumerate(string_name):
+    print(v)
